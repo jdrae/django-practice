e2871a7a534b1befb52a632bc3653c5d3e01ac24
jdrae <draejang@gmail.com>
2022-04-12 20:26:54 +0900

    settings & email app

---
+++ celeryproj/celeryproj/celery.py
@@ -0,0 +1,14 @@
+import os
+from celery import Celery
+from django.conf import settings
+
+# settings.py 의 환경설정 가져오기
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'celeryproj.settings')
+
+app = Celery('celeryproj')
+app.config_from_object('django.conf:settings', namespace='CELERY') # 환경설정 적용
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS) #  INSTALLED_APP 에 있는 장고 앱 자동으로 인식
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
---
+++ celeryproj/feedback/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import FormView
+
+from .forms import FeedbackForm
+
+
+class FeedbackView(FormView):
+    template_name = 'feedback/index.html'
+    form_class = FeedbackForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.send_email()
+        return super(FeedbackView, self).form_valid(form)
